22767675712e18794b24ce64ccfa932d132f7245
Neeraj Jain <neerajjain1392@gmail.com>
2019-09-25 14:46:29 +0530

    Project Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VehiclesComponent } from './vehicles/vehicles.component';
+import { MapComponent } from './map/map.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VehiclesComponent,
+    MapComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
